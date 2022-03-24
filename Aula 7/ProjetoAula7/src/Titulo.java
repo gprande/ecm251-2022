9619bfc5b9262e9225688746fa71960ca754e31f
gprande <bielprande@gmail.com>
2022-03-24 10:44:10 -0300

    Alteração Titulo.java

---
+++ Aula 7/ProjetoAula7/src/Titulo.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+
+public class Titulo {
+    private double valor;
+    private LocalDate data;
+    private double multaDiaria;
+
+    public Titulo(double valor, LocalDate data, double multaDiaria){
+        this.valor = valor;
+        this.data = data;
+        this.multaDiaria = multaDiaria;
+    }
+
+    public double getMultaDiaria() {
+        return multaDiaria;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+}
