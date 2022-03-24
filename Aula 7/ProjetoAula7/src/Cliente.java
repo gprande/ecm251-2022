71647a389de417b2bb59926ae90e4de85c1784ac
gprande <bielprande@gmail.com>
2022-03-24 10:26:00 -0300

    Alteração Aula 7

--- Aula 7/ProjetoAula7/src/Cliente.java
+++ Aula 7/ProjetoAula7/src/Cliente.java
@@ -2,13 +2,11 @@ public class Cliente {
     private String cpf;
     private String nome;
     private String email;
-    private Conta conta;
 
-    public Cliente(String nome, String cpf, String email, Conta conta){
+    public Cliente(String nome, String cpf, String email){
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
-        this.conta = conta;
     }
 
     public void visualizarCliente(){
@@ -16,7 +14,6 @@ public class Cliente {
         System.out.println("Nome:" + nome);
         System.out.println("CPF:" + cpf);
         System.out.println("E-mail:" + email);
-        System.out.println("E-mail:" + conta);
     }
 
     public String getNome(){
@@ -33,10 +30,6 @@ public class Cliente {
 
     public String getEmail(){
         return email;
-    }
-
-    public Conta getConta(){
-        return conta;
     }
 
    public void setEmail(String email){
--- Aula 7/ProjetoAula7/src/Sistema.java
+++ Aula 7/ProjetoAula7/src/Sistema.java
@@ -1,3 +1,7 @@
 public class Sistema {
-    
+    public void run(){
+        Cliente cliente = new Cliente("Midoria", "123456", "allmight_wannbe@gmail.com");
+        Conta conta = new Conta(cliente, 1234);
+        System.out.println(conta);
+    }
 }
