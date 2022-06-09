f2256d019154908cd1c6a541ad68378e74b612b9
gprande <bielprande@gmail.com>
2022-06-09 10:22:09 -0300

    Atividade 2

---
+++ Atividade 2/src/PostarMensagem.java
@@ -0,0 +1,6 @@
+public interface PostarMensagem {
+    String mobilemembers(Atividades atividade);
+    String heavylifters(Atividades atividade);
+    String scriptguys(Atividades atividade);
+    String bigbrothers(Atividades atividade);
+}
