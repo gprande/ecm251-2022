3cb8ae5ce3cd1962d53f4692d8c5a64be363a11f
gprande <bielprande@gmail.com>
2022-08-18 11:12:51 -0300

    Criação classe Boleto

--- Projeto Loja Online/Modelos/Pagamentos/Cartoes.py
+++ Projeto Loja Online/Modelos/Pagamentos/Cartoes.py
@@ -22,3 +22,9 @@ class Debito(Cartao):
         super().__init__(numero, titular, validade, cvv)
     def realizar_pagamento(self):
         return True 
+
+class Credito(Cartao):
+    def __init__(self, numero, titular, validade, cvv):
+        super().__init__(numero, titular, validade, cvv)
+    def realizar_pagamento(self):
+        return True
