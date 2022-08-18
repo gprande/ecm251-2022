747eae1ef5761d8c48048dcf4bfda96d539f51eb
gprande <bielprande@gmail.com>
2022-08-18 10:42:49 -0300

    Lojinha Online

---
+++ Projeto Loja Online/item.py
@@ -0,0 +1,24 @@
+class Item():
+    #Método Construtor
+    def __init__(self, nome, descricao, valor):
+        self._valor = valor
+        self._descricao = descricao
+        self._nome = nome
+    
+     #Demais métodos da classe
+    def get_valor(self):
+        return self._valor
+
+    def get_descricao(self):
+        return self._descricao
+
+    def get_nome(self):
+        return self._nome
+
+    def __str__(self) -> str:
+        return self._nome + ' ' + str(self._valor)
+
+    def __eq__(self, __o: object) -> bool:
+        if(isinstance(__o, Item)):
+            return self._nome == __o.get_nome()
+        return False
