273b879f5478bbf0b35fef35dc9f071a245d6ce0
Prandinho <bielprande@gmail.com>
2022-11-15 15:48:27 -0300

    Adaptação página login T3

---
+++ Projeto Loja Online T4/src/visual/register.py
@@ -0,0 +1,29 @@
+import streamlit as st
+
+def accept_register(new_user,new_email,new_password,new_cpf,new_birthdate,new_username):
+    if st.session_state["users_db"].add_user(new_user,
+                                            new_email,
+                                            new_password,
+                                            new_cpf,
+                                            new_birthdate,
+                                            new_username):
+        st.write("Registered successfully")
+    else:
+        st.write("Não foi possível realizar o cadastro, por favor, verifique seus dados.")
+
+class Register():
+    def __init__(self) -> None:       
+        new_username = st.text_input(label="Enter your name", key="new_username")
+        new_birthdate = st.text_input(label="Enter your birthdate", key="new_birthdate")
+        new_cpf = st.text_input(label="Enter your CPF (Only numbers):", key="new_cpf")
+        new_user = st.text_input(label="Enter your desired username :", key="new_user")
+        new_email = st.text_input(label="Enter your e-mail adress", key="new_email")
+        new_password = st.text_input(label="Type your desired password",type="password", key="new_password")
+        
+        if st.button("Register", key="register_button"):
+            accept_register(new_user,
+                            new_email,
+                            new_password,
+                            new_cpf,
+                            new_birthdate,
+                            new_username)
